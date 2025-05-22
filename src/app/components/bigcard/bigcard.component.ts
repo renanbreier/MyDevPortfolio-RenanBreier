2f9722fab6fce4b6e7de99fe45e94cdb29b31d67
Renan Breier <renanoliveirabreier@gmail.com>
2025-05-21 21:18:03 -0300

    Projeto inicial

--- src/app/components/bigcard/bigcard.component.ts
+++ src/app/components/bigcard/bigcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LandingButtonService } from 'src/app/service/landing-button.service';
 
 @Component({
@@ -6,16 +6,53 @@ import { LandingButtonService } from 'src/app/service/landing-button.service';
   templateUrl: './bigcard.component.html',
   styleUrls: ['./bigcard.component.css']
 })
-export class BigcardComponent {
-    showLinks = false;
-  
-    constructor(private LandingButtonService: LandingButtonService) {}
-  
-    toggleLinks(): void {
-      this.showLinks = !this.showLinks;
+export class BigcardComponent implements OnInit {
+  showLinks = false;
+
+  // Propriedades para animação de digitação
+  fullText = 'Bem-vindo ao meu portfólio';
+  displayedText = '';
+  typingSpeed = 100;
+  isDeleting = false;
+  loop = true;
+
+  constructor(private LandingButtonService: LandingButtonService) {}
+
+  ngOnInit(): void {
+    this.typeText();
+  }
+
+  toggleLinks(): void {
+    this.showLinks = !this.showLinks;
+  }
+
+  scrollToSection(sectionId: string): void {
+    this.LandingButtonService.scrollToSection(sectionId);
+  }
+
+  typeText(i: number = 0): void {
+    if (this.isDeleting) {
+      this.displayedText = this.fullText.substring(0, i - 1);
+      i--;
+    } else {
+      this.displayedText = this.fullText.substring(0, i + 1);
+      i++;
     }
-  
-    scrollToSection(sectionId: string): void {
-      this.LandingButtonService.scrollToSection(sectionId);
+
+    const delay = this.isDeleting ? 50 : this.typingSpeed;
+
+    if (!this.isDeleting && i === this.fullText.length) {
+      setTimeout(() => {
+        this.isDeleting = true;
+        this.typeText(i);
+      }, 1500);
+    } else if (this.isDeleting && i === 0) {
+      if (this.loop) {
+        this.isDeleting = false;
+        setTimeout(() => this.typeText(i), 500);
+      }
+    } else {
+      setTimeout(() => this.typeText(i), delay);
     }
+  }
 }
--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -14,13 +14,23 @@ export class ExperienceComponent {
       title: 'Analista de Suporte N3',
       company: 'Sonner Sistemas',
       date: '2023 - Presente',
-      description: 'Atuo na linha de frente como suporte técnico, atendendo Prefeituras, Câmaras e Autarquias. Participei de implantações, importações de dados, treinamentos e já atuei na liderança de uma equipe de suportes.'
+      description: 'Atuo na linha de frente como suporte técnico, atendendo Prefeituras, Câmaras e Autarquias.' +
+      'Minha atuação vai desde tarefas simples como a criação de usuários até tarefas mais complexas, como a configuração de web-services. ' + 
+      'Participo de implantações, importações de dados e treinamentos, além de atuar na liderança de uma equipe de suportes.\n' +
+      '<br>Principais atividades:\n- Atendimento de usuários e resolução de chamados N3\n- Implantação de sistemas de RH e Contabilidade\n- Treinamento e capacitação de usuários finais\n' +
+      '- Testes de correções, homologação e validação de novas funcionalidades\n- Atualização de bases de dados e acompanhamento de versões\n- Apoio na liderança de projetos e equipes de suporte técnico'
     },
     {
       title: 'Analista de Infraestrutura',
       company: 'Prefeitura Municipal da Estância de Socorro',
       date: '2021 - 2023',
-      description: 'Trabalhei no departamento de T.I, atuando na reestruturação da rede interna, montagem e manutenção de computadores e notebooks, configuração de servidores e switchs e controle de tickets.'
+      description: 'Trabalhei no departamento de tecnologia, onde tive ganhei uma experiência prática que foi fundamental para o início da minha trajetória profissional. ' +
+      'atuei na reestruturação da rede interna, montagem e manutenção de computadores e notebooks, configuração de servidores e switchs e também no controle de tickets.\n' +
+      '<br>Principais atividades:\n- Montagem e manutenção de computadores e notebooks\n - Instalação e formatação de sistemas operacionais\n- Monitoramento e manutenção da rede de fibra óptica (Zabbix)\n' +
+      '- Controle e atendimento de chamados técnicos (GLPI)\n- Instalação e configuração de servidores (Linux e Windows)'
     }
-  ];
+  ].map(exp => ({
+    ...exp,
+    description: exp.description.replace(/\n/g, '<br>')
+  }));
 }

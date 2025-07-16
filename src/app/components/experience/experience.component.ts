1dbbfc2a29f3a0efe424d6748f151dc37b8f4849
Renan Breier <renanoliveirabreier@gmail.com>
2025-07-15 21:56:26 -0300

    fix: experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -14,20 +14,15 @@ export class ExperienceComponent {
       title: 'Analista de Suporte N3',
       company: 'Sonner Sistemas',
       date: '2023 - Presente',
-      description: 'Atuo na linha de frente como suporte técnico, atendendo Prefeituras, Câmaras e Autarquias. ' +
-      'Minha atuação vai desde tarefas simples como a criação de usuários até tarefas mais complexas, como a configuração de web-services. ' + 
-      'Participo de implantações, importações de dados e treinamentos, além de atuar na liderança de uma equipe de suportes.\n' +
-      '<br>Principais atividades:\n- Atendimento de usuários e resolução de chamados N3\n- Implantação de sistemas de RH e Contabilidade\n- Treinamento e capacitação de usuários finais\n' +
-      '- Testes de correções, homologação e validação de novas funcionalidades\n- Atualização de bases de dados e acompanhamento de versões\n- Apoio na liderança de projetos e equipes de suporte técnico'
+      description: 'Atuo na linha de frente do suporte técnico, prestando atendimento especializado a servidores públicos de Prefeituras, Câmaras Municipais e Autarquias que utilizam os sistemas da Sonner. Minha atuação abrange desde demandas operacionais até configurações técnicas avançadas, como a integração de sistemas via web services.\n\n'
+      + 'Tenho experiência em liderança de equipes de suporte, implantação de sistemas corporativos, migração e conversão de dados, além de ministrar treinamentos presenciais e remotos em larga escala. Um dos marcos da minha trajetória foi a coordenação do desenvolvimento e implantação do aplicativo "Cidadão Online" no município de Socorro, contribuindo para a transformação digital dos serviços públicos locais.'
     },
     {
-      title: 'Analista de Infraestrutura',
+      title: 'Jovem Aprendiz de T.I',
       company: 'Prefeitura Municipal da Estância de Socorro',
       date: '2021 - 2023',
-      description: 'Trabalhei no departamento de tecnologia, onde tive ganhei uma experiência prática que foi fundamental para o início da minha trajetória profissional. ' +
-      'atuei na reestruturação da rede interna, montagem e manutenção de computadores e notebooks, configuração de servidores e switchs e também no controle de tickets.\n' +
-      '<br>Principais atividades:\n- Montagem e manutenção de computadores e notebooks\n - Instalação e formatação de sistemas operacionais\n- Monitoramento e manutenção da rede de fibra óptica (Zabbix)\n' +
-      '- Controle e atendimento de chamados técnicos (GLPI)\n- Instalação e configuração de servidores (Linux e Windows)'
+      description: 'Atuei no setor de Tecnologia da Informação da Prefeitura, onde tive uma experiência prática essencial para o início da minha trajetória profissional na área. Durante esse período, desenvolvi competências técnicas e interpessoais fundamentais, que reforçaram minha vocação para a área de TI e me prepararam para desafios mais complexos no futuro.\n\n'
+      + 'Minhas atividades abrangeram desde tarefas de infraestrutura básica até intervenções em redes de fibra óptica e configuração de servidores. Essa vivência me proporcionou uma base sólida em suporte técnico, redes, manutenção e administração de sistemas.'
     }
   ].map(exp => ({
     ...exp,

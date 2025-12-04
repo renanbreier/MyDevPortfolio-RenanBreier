0e8cfa8a7bd663a8fd62585dc7725e9a9f771af6
Renan Breier <renanoliveirabreier@gmail.com>
2025-12-03 23:41:08 -0300

    Projects & Skills changelog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,8 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { BigcardComponent } from './components/bigcard/bigcard.component';
 import { HomeComponent } from './pages/home/home.component';
-import { TechcardComponent } from './components/techcard/techcard.component';
 import { AboutComponent } from './components/about/about.component';
-import { ImgCardComponent } from './components/techcard/img-card/img-card.component';
 import { ButtonComponent } from './components/atom/button/button.component';
-import { ProjectsCardComponent } from './components/projects-card/projects-card.component';
-import { CardsComponent } from './components/projects-card/cards/cards.component';
-import { CardPhotoComponent } from './components/projects-card/cards/card-photo/card-photo.component';
-import { CardLabelComponent } from './components/projects-card/cards/card-label/card-label.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { InputComponent } from './components/atom/input/input.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,14 +26,8 @@ import { ProjectsComponent } from './components/projects/projects.component';
     MenuBarComponent,
     BigcardComponent,
     HomeComponent,
-    TechcardComponent,
     AboutComponent,
-    ImgCardComponent,
     ButtonComponent,
-    ProjectsCardComponent,
-    CardsComponent,
-    CardPhotoComponent,
-    CardLabelComponent,
     ContactComponent,
     InputComponent,
     BackToTopDirective,
